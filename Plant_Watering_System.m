2da6526e1cf8925b0a2b0355fb44a70c04fa33ac
NutmegCat <deltaechogames@gmail.com>
2024-10-15 22:36:27 -0400

    LED lights up when water is needed, also hid the numbers with vars

--- Plant_Watering_System.m
+++ Plant_Watering_System.m
@@ -5,29 +5,40 @@ clc
 
 a = arduino('COM5', 'Nano3');
 
+% watering values
+reallyDryValue = 3.5;
+moistureThreshold = 2.7;
+saturaedValue = 2.7;
+
 % stop condition
 stop = 0;
 
 % beginning stop loop
 while ~stop
-    
+
     dryness = readVoltage(a, 'A1'); % variable for moisture sensor voltage
 
     % conditional for dry soil
-    if (dryness > 3.5)
+    if (dryness > reallyDryValue)
         plant_state = "Thirsty";
         disp(plant_state);
         writeDigitalPin(a, 'D2', 1)
+        writeDigitalPin(a, 'D4', 1)
+        
     % conditional for semi-wet soil
-    elseif (dryness > 2.7)
+    elseif (dryness > moistureThreshold)
         plant_state = "A bit of water is needed";
         disp(plant_state);
         writeDigitalPin(a, 'D2', 1)
+        writeDigitalPin(a, 'D4', 1)
+        
     % conditional for wet soil
-    elseif (dryness <= 2.7)
+    elseif (dryness <= saturaedValue)
         plant_state = "Watered";
         disp(plant_state);
         writeDigitalPin(a, 'D2', 0)
+        writeDigitalPin(a, 'D4', 0)
+
     % conditional if failure
     else
         plant_state = "SYSTEM FAILURE";
